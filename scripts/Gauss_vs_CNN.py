ac934f28832ef1e7ab95a9772dd79d5cd6c50230
Tanveer <tanveer.karim@cfa.harvard.edu>
2019-04-22 11:19:52 -0400

    update 22-Apr-19

--- scripts/Gauss_vs_CNN.py
+++ scripts/Gauss_vs_CNN.py
@@ -8,36 +8,30 @@ import matplotlib.pyplot as plt
 plt.style.use("fivethirtyeight")
 
 #read Jae's data; this is saved in results/JaeResults/union-catalog-results/
-data_jae = np.load("../results/JaeResults/union-catalog-results.npy").item()
+df_CNN = pd.read_csv("../data/JaeResults/union-catalog.csv")
 
-#user inputs actual name of mask; format available in data_jae['MASK_NAMES']
+#user inputs actual name of mask
 maskname = sys.argv[1]
-MMT_maskID_b = sys.argv[1] #refers to the four digit code of the bluemask; can be found in docs/folder_to_mask.txt
-
-#find mask's number in Jae's data file
-for i in range(len(data_jae['MASK_NAMES'])):
-    if(data_jae['MASK_NAMES'][i] == maskID):
-        mask_num = i #mask_num corresponds to maskID in Jae's file
-        break
+MMT_maskID_b = sys.argv[2]
 
 #filter out relevant mask and turn it into a pandas dataframe
-jae_df = pd.DataFrame(data_jae['REDZ'][data_jae['MASK_NUM'] == mask_num], columns=['jae_z'])
-jae_df = jae_df[1:-1] #clip first and last point to match Gaussian_Filter data length
+df_CNN = df_CNN[df_CNN['mask_name'] == maskname]
+df_CNN = df_CNN[1:-1] #clip first and last point to match Gaussian_Filter data length
 
 #turn -999 to np.nan
 replace_999 = {-999:np.nan}
-jae_df = jae_df.replace(replace_999)
+df_CNN = df_CNN.replace(replace_999)
 
 #read in Gaussian filter results
-tanveer_df = pd.read_excel("../results/Max_z_n_width/" + MMT_maskID_b + "+" +\
+df_Gauss = pd.read_excel("../results/Max_z_n_width/" + MMT_maskID_b + "+" +\
  str(int(MMT_maskID_b) + 1) + "_visual-inspected.xlsx")
 
 #take average between blue and red mask and generate final redz
-tanveer_df['z_final'] = tanveer_df[['z_b', 'z_r']].mean(axis = 1)
+df_Gauss['z_final'] = df_Gauss[['z_b', 'z_r']].mean(axis = 1)
 
 #redshift arrays
-jz = np.array(jae_df['jae_z'])
-tz = np.array(tanveer_df['z_final'])
+jz = np.array(df_CNN['redz'])
+tz = np.array(df_Gauss['z_final'])
 
 #plot comparison
 x = np.arange(0, 1.7, .1)
@@ -46,12 +40,34 @@ plt.plot(jz, tz, marker = "o", ls = "", alpha = alpha)
 plt.plot(x, x, alpha = alpha)
 plt.xlabel("CNN redshifts")
 plt.ylabel("Gaussian redshifts")
-fname = "../results/summary_statistics/" + maskname + "CNN_vs_Gauss_comparison.png"
-plt.plot(fname, dpi = 250, bbox_inches = 'tight')
+fname = "../results/summary_statistics/CNN_vs_Gauss/" + maskname \
++ "-CNN_vs_Gauss_comparison.png"
+plt.savefig(fname, dpi = 250, bbox_inches = 'tight')
 
 #check if there are any discrepancies over 0.01 in redshift
 idx = np.where(jz-tz > 0.01)
-discrepant_slits = tanveer_df.iloc[idx]
+discrepant_slits_G = df_Gauss.iloc[idx]
+discrepant_slits_C = df_CNN.iloc[idx]
+discrepant_slits = pd.concat([discrepant_slits_G, discrepant_slits_C], axis = 1)
 
 #show user list of discrepancies
+print('discrepant slits:')
+print('-----------------')
 print(discrepant_slits)
+print("------------------------------------------------------")
+
+#Show stats on how many redshifts CNN method did not identify
+idx = np.where(np.isnan(jz))
+print("slits where Gaussian filter found redz but CNN did not:")
+print("Total number of slits: ", np.sum((np.isnan(tz[np.isnan(jz)]) == False)))
+print("------------------------------------------------------")
+print(df_Gauss.iloc[idx].dropna(subset = ['z_final']))
+print("------------------------------------------------------")
+
+#Show stats on how many redshifts Gaussian Filter method did not identify
+idx = np.where(np.isnan(tz))
+print("slits where CNN found redz but Gaussian Filter did not:")
+print("Total number of slits: ", np.sum((np.isnan(jz[np.isnan(tz)]) == False)))
+print("------------------------------------------------------")
+print(df_CNN.iloc[idx].dropna())
+print("------------------------------------------------------")
