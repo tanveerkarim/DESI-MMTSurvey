ce60db47ab2ea459844c4e189af13ea33415355d
Tanveer <tanveer.karim@cfa.harvard.edu>
2019-06-27 14:01:06 -0400

    initialize script

---
+++ scripts/update_velocityModel.py
@@ -0,0 +1,6 @@
+This script looks at excel notebook, recalculates the 
+velocity and amplitude based on the correct redshift.
+
+This requires a modification of SNR_calculator() which ONLY
+searches over the width space, and NOT the redshift space.
+
